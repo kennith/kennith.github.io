2bf43b20af2baba49cdf8b5ec4ebdae448ded733
Kennith Leung <kennith.leung@gmail.com>
2016-07-03 08:44:16 -0700

    Add browsersync options

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var bs   = require('browser-sync');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,7 +10,15 @@ gulp.task('default', function() {
  *
  * This task rebuild the jekyll site when a post is updated.
  * Jekyll automatically rebuild the site when something change. 
+ * Needs to watch the changes in site?
  */
 gulp.task('watch', function() {
-
+	browserSync.init({
+	    proxy: "127.0.0.1:4000",
+	    online: false,
+	    logPrefix: 'JEKYLL',
+	    open: false,
+	    ui: false,
+	});
+	gulp.watch('_site/feed.xml').on('change', browserSync.reload);
 });
