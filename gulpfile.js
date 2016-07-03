113aec91a5629c6c497ea0d133df118a5f030bb3
Kennith Leung <kennith.leung@gmail.com>
2016-07-03 08:17:30 -0700

    Add gulp and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var bs   = require('browser-sync');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+/**
+ * gulp watch
+ *
+ * This task rebuild the jekyll site when a post is updated.
+ * Jekyll automatically rebuild the site when something change. 
+ */
+gulp.task('watch', function() {
+
+});
