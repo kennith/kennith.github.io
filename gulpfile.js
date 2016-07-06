eae12b97cbd40e3b7c3f154f146c4c0bc3b569f1
Kennith Leung <kennith.leung@gmail.com>
2016-07-06 15:12:43 -0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  // place code for your default task here
+// place code for your default task here
 });
 
+
+gulp.task('bootstrap:update', function() {
+	var js = [
+		'./bower_components/jquery/dist/jquery.min.js',
+		'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+	];
+	return gulp.src(js)
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./javascripts/'))
+})
 /**
- * gulp watch
- *
- * This task rebuild the jekyll site when a post is updated.
- * Jekyll automatically rebuild the site when something change. 
- * Needs to watch the changes in site?
- */
+* gulp watch
+*
+* This task rebuild the jekyll site when a post is updated.
+* Jekyll automatically rebuild the site when something change. 
+* Needs to watch the changes in site?
+*/
 gulp.task('watch', function() {
 	browserSync.init({
-	    proxy: "127.0.0.1:4000",
-	    online: false,
-	    logPrefix: 'JEKYLL',
-	    open: false,
-	    ui: false,
+		proxy: "127.0.0.1:4000",
+		online: false,
+		logPrefix: 'JEKYLL',
+		open: false,
+		ui: false,
 	});
 	gulp.watch('_site/feed.xml').on('change', browserSync.reload);
 });
